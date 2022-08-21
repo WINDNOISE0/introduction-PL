7081e1ec862cd8210c02cb02ad002e918434f7fc
Jaroslav <jarileslava@gmail.com>
2022-08-21 17:20:18 +0300

    Add comparing numbers

---
+++ Exampel006/Program.cs
@@ -0,0 +1,13 @@
+﻿int a = 2;
+int b = 10;
+int c = 1;
+int d = 4;
+
+int max = a;
+
+if (max < b) max = b;
+if (max < c) max = c;
+if (max < d) max = d;
+
+Console.Write("max = ");
+Console.WriteLine(max);
